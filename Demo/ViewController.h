bbb53028046092907e86e1b25da933c0b61fdb2e
Ryan <yanyingxiang@gmail.com>
2015-01-20 20:17:05 +0800

    Initial Commit

---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Ryan on 15/1/20.
+//  Copyright (c) 2015年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo
+//
+//  Created by Ryan on 15/1/20.
+//  Copyright (c) 2015年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
